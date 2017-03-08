8e0db8809f22592483f04659dedb892a94ec69ce
mariot <mariotjimenez@gmail.com>
2017-03-08 11:43:00 -0600

    Added description

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,31 @@
-
+#!/usr/bin/env python3
 # Mario Jimenez
-# Tokenizer
+# Tokenizer - Algorithms Project
+
+specialCharacters = ['<', '>', '<=', '>=', '=', '*', '+', '-', '^', '|', '~',
+                     '=>', '<=>', '.', ',', '{', '}', '(', ')', '..', ']',
+                     '[', '/', '\\']
+
+stringCharacters = ['\\\\', '\\t', '\\r', '\\n', '\\\'']
+
+whitespaceCharacters = [' ', '\\t', '\\r', '\\n']
+
+
+def whiteChar(c):
+    return (c in " \\r\n\t\v")
+
+
+print(whiteChar("\\r"))x
+
+# for x in stringCharacters:
+#     print(x)
+
+
+def tokenize(String: str):
+
+    return str
+
+
+# inputString = input("Enter String:\n")
+
+# print(tokenize(inputString))
